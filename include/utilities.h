30f0f16b8ceeedc129dc7f13be2a92fd19460c8a
Carmelo Evoli <carmelo.evoli@me.com>
2017-07-17 22:48:45 +0200

    add utilities

---
+++ include/utilities.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_UTILITIES_H_
+#define INCLUDE_UTILITIES_H_
+
+namespace DRAGON {
+
+}
+
+#endif /* INCLUDE_UTILITIES_H_ */
---
+++ src/utilities.cpp
@@ -0,0 +1,5 @@
+#include "utilities.h"
+
+namespace DRAGON {
+
+} /* namespace DRAGON */
