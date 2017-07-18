08a12f6e1633bc28ba140201617582646031d85f
Carmelo Evoli <carmelo.evoli@me.com>
2017-07-18 21:32:58 +0200

    add Nakanishi03 HI model

--- include/gas_HI.h
+++ include/gas_HI.h
@@ -21,6 +21,23 @@ private:
 	Ferriere07::HI gas;
 };
 
+class HI_Nakanishi03: public Galaxy {
+public:
+	HI_Nakanishi03() {
+	}
+	std::string description() const override {
+		return "HI_Nakanishi03";
+	}
+	std::shared_ptr<Galaxy> clone() const override {
+		return std::make_shared<HI_Nakanishi03>(*this);
+	}
+	double get(const Vector3d& pos) const override;
+private:
+	double h_0 = 1.06 * pc;
+	double n_0 = 0.94 / cm3;
+};
+
+
 } /* namespace DRAGON */
 
 #endif /* INCLUDE_GAS_HI_H_ */
--- src/gas_HI.cpp
+++ src/gas_HI.cpp
@@ -9,4 +9,15 @@ double HI_Ferriere07::get(const Vector3d& pos) const {
 	return gas.density(pos_pc.x, pos_pc.y, pos_pc.z) / cm3;
 }
 
+/*** HI_Nakanishi03 ***/
+
+double HI_Nakanishi03::get(const Vector3d& pos) const {
+	double r_kpc = pos.getR() / kpc;
+	double exp_1 = std::exp(-r_kpc / 2.4);
+	double exp_2 = std::exp(-pow2((r_kpc - 9.5) / 4.8));
+	double densityOnPlane = n_0 * (0.6 * exp_1 + 0.24 * exp_2);
+	double scaleHeight = h_0 * (116.3 + 19.3 * r_kpc + 4.1 * pow2(r_kpc) - 0.05 * pow3(r_kpc));
+	return densityOnPlane * std::exp(-M_LN2 * pow2(pos.z / scaleHeight));
+}
+
 } /* namespace DRAGON */
