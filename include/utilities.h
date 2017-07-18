b69476493ae42edeabd83596ebb36741114712a0
Carmelo Evoli <carmelo.evoli@me.com>
2017-07-18 10:20:35 +0200

    Function to multiply vector

--- include/utilities.h
+++ include/utilities.h
@@ -1,7 +1,12 @@
 #ifndef INCLUDE_UTILITIES_H_
 #define INCLUDE_UTILITIES_H_
 
+#include <vector>
+
 namespace DRAGON {
+
+void copy_vector_with_units(const std::vector<double>& v_old,
+		std::vector<double>& v_new, double units = 1.0);
 
 }
 
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -2,4 +2,10 @@
 
 namespace DRAGON {
 
+void copy_vector_with_units(const std::vector<double>& v_old,
+		std::vector<double>& v_new, double units) {
+	std::transform(v_old.begin(), v_old.end(), std::back_inserter(v_new),
+			[units](double a) -> double {return a * units;});
+}
+
 } /* namespace DRAGON */
