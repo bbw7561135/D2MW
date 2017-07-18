bbd487467522eaccc7240055e2c0c5c22e406e88
Carmelo Evoli <carmelo.evoli@me.com>
2017-07-18 10:39:35 +0200

    Add Bronfmann88 model

--- include/gas_H2.h
+++ include/gas_H2.h
@@ -1,7 +1,11 @@
 #ifndef INCLUDE_GAS_H2_H_
 #define INCLUDE_GAS_H2_H_
 
+#include <algorithm>
+#include <vector>
+
 #include "galaxy.h"
+#include "utilities.h"
 #include "models/gasFerriere2007.h"
 
 namespace DRAGON {
@@ -19,6 +23,26 @@ public:
 	double get(const Vector3d& pos) const override;
 private:
 	Ferriere07::H2 gas;
+};
+
+class H2_Bronfman88: public Galaxy {
+public:
+	H2_Bronfman88() {
+		init();
+	}
+	std::string description() const override {
+		return "H2_Bronfman88";
+	}
+	std::shared_ptr<Galaxy> clone() const override {
+		return std::make_shared<H2_Bronfman88>(*this);
+	}
+	double get(const Vector3d& pos) const override;
+private:
+	void init();
+	std::vector<double> R;
+	std::vector<double> n0;
+	std::vector<double> Z0;
+	std::vector<double> Zh;
 };
 
 } /* namespace DRAGON */
