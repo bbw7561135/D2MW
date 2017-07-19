dd980088ec7658233eca8257e733d47388bbf623
Carmelo Evoli <carmelo.evoli@me.com>
2017-07-19 22:33:16 +0200

    Add documentation

--- include/gas_HI.h
+++ include/gas_HI.h
@@ -6,16 +6,34 @@
 
 namespace DRAGON {
 
+/**
+ * Class to implement the Ferriere2007 model for HI
+ */
 class HI_Ferriere07: public Galaxy {
 public:
+	/**
+	 Class Constructor
+	 */
 	HI_Ferriere07() {
 	}
+	/**
+	 * Get the reference for the Ferriere2007 model
+	 * @return string containing a reference
+	 */
 	std::string description() const override {
 		return "HIFerriere07";
 	}
+	/**
+	 * Get the pointer of this istance
+	 */
 	std::shared_ptr<Galaxy> clone() const override {
 		return std::make_shared<HI_Ferriere07>(*this);
 	}
+	/**
+	 * Gas density at the given position
+	 * @param pos Galactic position in mks
+	 * @return number density in mks
+	 */
 	double get(const Vector3d& pos) const override;
 private:
 	Ferriere07::HI gas;
@@ -36,7 +54,6 @@ private:
 	double h_0 = 1.06 * pc;
 	double n_0 = 0.94 / cm3;
 };
-
 
 } /* namespace DRAGON */
 
