25a98fd1fff875c147cec630be17ae7267b1d695
Carmelo Evoli <carmelo.evoli@me.com>
2017-08-02 15:25:33 +0200

    fixing Ferriere07 model

--- include/models/gas_Ferriere2007.h
+++ include/models/gas_Ferriere2007.h
@@ -3,45 +3,10 @@
 
 #include <algorithm>
 #include <cmath>
+#include "models/gravitational_potential.h"
 #include "mathutils.h"
 
 namespace Ferriere07 {
-
-/**
- * Class to compute the galactic gravitational potential
- */
-class GravitationalPotential {
-public:
-	/**
-	 * Default constructor
-	 */
-	GravitationalPotential() {
-	}
-
-	/**
-	 * Destructor
-	 */
-	virtual ~GravitationalPotential() {
-	}
-
-	/**
-	 * Compute the gravitational potential
-	 * @param r r-position in pc
-	 * @param z z-position in pc
-	 * @return gravitational potential in (km/s)^2
-	 */
-	double get(const double& r, const double& z) const;
-
-protected:
-	double C1 = 8.887e3;
-	double C2 = 3.e3;
-	double C3 = 0.325;
-	double a1 = 6.5e3;
-	double a2 = 0.7e3;
-	double a3 = 12e3;
-	double b1 = 0.26e3;
-	double rh = 210e3;
-};
 
 class Gas {
 protected:
